ff240a353e25d3a0891e093b2333f21e85b1c894
rakib153 <mdkhairuzzamanrakib153@gmail.com>
2023-11-15 04:00:32 +0600

    Add project files.

---
+++ Project1/Controllers/CandidatesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly CandidateDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public CandidatesController(CandidateDbContext context, IWebHostEnvironment env)
+        {
+            this._context = context;
+            this._env = env;
+        }
+        [HttpGet]
+        [Route("GetSkills")]
+        public async Task<ActionResult<IEnumerable<Skill>>> GetSkills()
+        {
+            return await _context.Skills.ToListAsync();
+        }
+        [HttpGet]
+        [Route("GetCandidates")]
+        public async Task<ActionResult<IEnumerable<Candidate>>> GetCandidate()
+        {
+            return await _context.Candidates.ToListAsync();
+        }
+
+
+    }
+}
---
+++ Project1/Models/ViewModels/CandidateVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models.ViewModels
+{
+    public class CandidateVM
+    {
+        public int CandidateId { get; set; }
+        [Required, StringLength(50), Display(Name = "Candidate Name")]
+        public string CandidateName { get; set; } = default!;
+        [Required, Display(Name = "Date of Birth"), DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        public string Picture { get; set; } = default!;
+        public IFormFile PictureFile { get; set; } = default!;
+        public bool Fresher { get; set; }
+        public string SkillsStringfy { get; set; } = default!;
+        public Skill[]? SkillList { get; set; }
+    }
+}
